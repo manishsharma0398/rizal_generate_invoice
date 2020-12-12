71e27de2c1099ae5775d4bb0a5c5d1cf9c282f57
Manish Sharma <manish.sharma0398@gmail.com>
2020-12-12 22:20:15 +0530

    redux initial setup done

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./root-reducer";
+const middleware = [];
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
---
+++ src/redux/user/user-reducer.js
@@ -0,0 +1,17 @@
+import { SET_CURRENT_USER } from "../types";
+
+const initialState = {
+  currentUser: null,
+};
+
+const userReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
